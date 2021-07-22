7fe23ee2ff91844ce4b28ab0e19a8742a3b86018
Andrey Novikov <envek@envek.name>
2021-07-22 16:08:31 +0300

    `bundle gem yabeda-anycable`

---
+++ lib/yabeda/anycable.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "anycable/version"
+
+module Yabeda
+  module Anycable
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
