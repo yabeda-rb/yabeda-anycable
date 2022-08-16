dbab93ec58731552b43758cc32bca15a36160cfe
Tomaz Zlender <tomaz.zlender@gmail.com>
2022-08-16 10:34:05 +0200

    When a response doesn't respond to :status we assume the status is ERROR
    When a response yields an error, the value of the response variable is `nil` instead of `AnyCable::CommandResponse`.

--- lib/yabeda/anycable/middleware.rb
+++ lib/yabeda/anycable/middleware.rb
@@ -11,7 +11,7 @@ module Yabeda
         (response = yield)
       ensure
         elapsed = (Process.clock_gettime(Process::CLOCK_MONOTONIC) - started).round(4)
-        labels = { method: rpc_method_name.to_s, status: response.status.to_s }
+        labels = { method: rpc_method_name.to_s, status: response.respond_to?(:status) ? response.status.to_s : "ERROR" }
         labels[:command] = request.respond_to?(:command) ? request.command : ""
         ::Yabeda.anycable.rpc_call_count.increment(labels)
         ::Yabeda.anycable.rpc_call_runtime.measure(labels, elapsed)
